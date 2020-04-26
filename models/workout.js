a2852dd411ed53d5767a26bdebe390131500f6d5
Tami Shute <tamaralynnrenee@gmail.com>
2020-04-26 16:18:42 -0400

    initialize files

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+     type: Date,
+      default: Date.now}, /*new Date().setDate(new Date().getDate()-10), /***Change to date.now*/
+  exercises: [
+    {
+      type: {
+        type: String, required: true
+      },
+      name: {
+        type: String, required: true
+      },
+      duration: {
+        type: Number, required: true
+      },
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
